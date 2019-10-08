51ce0fe0b9303adb3356e62bd3786e74d23e5aed
Subrat Rudra <ssubrat.rrudra@gmail.com>
2019-10-09 02:28:00 +0530

    Update StringToListSplitter.java

--- StringToListSplitter/src/main/java/com/subrlibr/stringtolistsplitter/StringToListSplitter.java
+++ StringToListSplitter/src/main/java/com/subrlibr/stringtolistsplitter/StringToListSplitter.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class StringToListSplitter {
 
-    public static List<String> splitIt(String stringToBeSplit, String stringAsSplitPoint) {
+    public static List<String> splitIt(String stringToBeSplit, String splitBy) {
         List<String> listSplitString = new ArrayList<>();
-        if (stringToBeSplit.contains(stringAsSplitPoint)) {
-            String[] stringsSplit = stringToBeSplit.split(stringAsSplitPoint);
+        if (stringToBeSplit.contains(splitBy)) {
+            String[] stringsSplit = stringToBeSplit.split(splitBy);
             for (int i = 0; i < stringsSplit.length; i++) {
                 listSplitString.add(stringsSplit[i]);
             }
